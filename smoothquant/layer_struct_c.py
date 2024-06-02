Pass pipeline but wrong result

smoothquant/layer_struct_c.py
@@ -84,17 +84,15 @@ class LayerStructC:
     @classmethod
     def Get_Encrypted_Tensor_Opr2_Int32(cls, src_id1, src_id2):
         B, M, K = cls.Get_Tensor_Dim_Int32(src_id1)
-        _, N, K = cls.Get_Tensor_Dim_Int32(src_id2)
+        _, N, _ = cls.Get_Tensor_Dim_Int32(src_id2)
 
         enc_x = torch.empty((B, M, K), dtype=torch.int32)
         enc_y = torch.empty((B, N, K), dtype=torch.int32) 
 
         blind_factor_ids = torch.empty(2, dtype=torch.int32)
 
-        start_time = time.perf_counter_ns()
         cls.lib.Ex_Get_Encrypted_Tensor_Opr2_Int32(src_id1, src_id2, cast(
             enc_x.data_ptr(), POINTER(c_int32)), cast(enc_y.data_ptr(), POINTER(c_int32)), cast(blind_factor_ids.data_ptr(), POINTER(c_int32)))
-        print(f"Ex_Get_Encrypted_Tensor_Opr2_Int32 End-to-end Latency: {(time.perf_counter_ns() - start_time)/1e9:0.6f} s")
         return enc_x, enc_y, blind_factor_ids[0], blind_factor_ids[1]
 
     @classmethod
@@ -103,13 +101,60 @@ class LayerStructC:
 
     @classmethod
     def Set_Decrypted_Tensor_Opr2_Int32(cls, src, decryption_key_id):
-        start_time = time.perf_counter_ns()
         ret_id = cls.lib.Ex_Set_Decrypted_Tensor_Opr2_Int32(cast(src.data_ptr(), POINTER(c_int32)),
                                                           src.size(0), src.size(
                                                               1), src.size(2),
                                                           decryption_key_id)
-        print(f"Ex_Set_Decrypted_Tensor_Opr2_Int32 End-to-end Latency: {(time.perf_counter_ns() - start_time)/1e9:0.6f} s")
         return ret_id
+    
+
+    @classmethod
+    def Get_Encrypted_Tensor_QK_Int32_KV_Cache_Opt(cls, src_id1, src_id2):
+        B, M, K = cls.Get_Tensor_Dim_Int32(src_id1)
+        _, N, K2 = cls.Get_Tensor_Dim_Int32(src_id2)
+
+        enc_x = torch.empty((B, M, K), dtype=torch.int32)
+        enc_y = torch.empty((B, N, K2), dtype=torch.int32)
+
+        cls.lib.Ex_Get_Encrypted_Tensor_QK_Int32_KV_Cache_Opt(src_id1, src_id2, cast(enc_x.data_ptr(), POINTER(c_int32)), cast(enc_y.data_ptr(), POINTER(c_int32)))
+        return enc_x, enc_y
+    
+    @classmethod
+    def Generate_Decryption_Key_QK_Int32_KV_Cache_Opt(cls, src_id1, src_id2):
+        return cls.lib.Ex_Generate_Decryption_Key_QK_Int32_KV_Cache_Opt(src_id1, src_id2)
+
+    @classmethod
+    def Set_Decrypted_Tensor_QK_Int32_KV_Cache_Opt(cls, src, decryption_key_id):
+        return cls.lib.Ex_Set_Decrypted_Tensor_QK_Int32_KV_Cache_Opt(cast(src.data_ptr(), POINTER(c_int32)),
+                                                          src.size(0), src.size(
+                                                              1), src.size(2),
+                                                          decryption_key_id)
+    
+    @classmethod
+    def Get_Encrypted_Tensor_PV_Int32_KV_Cache_Opt(cls, src_id1, src_id2):
+        B, M, K = cls.Get_Tensor_Dim_Int32(src_id1)
+        _, N, K2 = cls.Get_Tensor_Dim_Int32(src_id2)
+
+        enc_x = torch.empty((B, M, K), dtype=torch.int32)
+        enc_y = torch.empty((B, N, K2), dtype=torch.int32)
+
+        cls.lib.Ex_Get_Encrypted_Tensor_PV_Int32_KV_Cache_Opt(src_id1, src_id2, cast(enc_x.data_ptr(), POINTER(c_int32)), cast(enc_y.data_ptr(), POINTER(c_int32)))
+        return enc_x, enc_y
+    
+    @classmethod
+    def Generate_Decryption_Key_PV_Int32_KV_Cache_Opt(cls, src_id1, src_id2):
+        return cls.lib.Ex_Generate_Decryption_Key_PV_Int32_KV_Cache_Opt(src_id1, src_id2)
+    
+    @classmethod
+    def Set_Decrypted_Tensor_PV_Int32_KV_Cache_Opt(cls, src, decryption_key_id):
+        return cls.lib.Ex_Set_Decrypted_Tensor_PV_Int32_KV_Cache_Opt(cast(src.data_ptr(), POINTER(c_int32)),
+                                                          src.size(0), src.size(
+                                                              1), src.size(2),
+                                                          decryption_key_id)
+
+    @classmethod
+    def Pre_Init(cls):
+        return cls.lib.Ex_Pre_Init()
 
     @classmethod
     def Compute_Epilogue_WS8BS8(cls, src_id, linear_param_id):

smoothquant/my_bmm.py
@@ -23,6 +23,7 @@ class BMM_S8X_S8Y_FP32Z_Mixed:
     def __init__(self, torch_int_nn_bmm, privacy_on, module_name=None):
         self.privacy_on = privacy_on
         self.module_name = module_name
+        self.is_pv_bmm = True if module_name == "PV BMM" else False
         # print(f'{module_name} is created')
 
         if smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode4:
@@ -32,7 +33,7 @@ class BMM_S8X_S8Y_FP32Z_Mixed:
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode6:
             self.sgx_lsc = sgx_lsc.SgxLayerStructC()
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode7:
-            self.sgx_lsc = sgx_lsc.SgxLayerStructC()
+            self.lsc = lsc.LayerStructC()
 
         if smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode3:
             self.alpha = torch.tensor(
@@ -44,7 +45,11 @@ class BMM_S8X_S8Y_FP32Z_Mixed:
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode6:
             self.bmm_id = self.sgx_lsc.Set_Bmm_Param(torch_int_nn_bmm)
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode7:
-            self.bmm_id = self.sgx_lsc.Set_Bmm_Param(torch_int_nn_bmm)
+            self.bmm_id = self.lsc.Set_Bmm_Param(torch_int_nn_bmm)
+
+        if smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode7:
+            self.cache = None
+
 
     def __run(self, x, y):
         state = 'Prefill' if smoothquant.opt.is_prefill else 'Generation'
@@ -68,8 +73,8 @@ class BMM_S8X_S8Y_FP32Z_Mixed:
             y = self.sgx_lsc.Cast_From_Int8_To_Int32(y)
             # y is transposed inside
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode7:
-            x = self.sgx_lsc.Cast_From_Int8_To_Int32(x)
-            y = self.sgx_lsc.Cast_From_Int8_To_Int32(y)
+            x = self.lsc.Cast_From_Int8_To_Int32(x)
+            y = self.lsc.Cast_From_Int8_To_Int32(y)
             # y is transposed inside
         else:
             assert False
@@ -77,26 +82,26 @@ class BMM_S8X_S8Y_FP32Z_Mixed:
 
         t = timer.start(tag=f'{self.module_name}, Process Input Tensors Before Offload ({state})', category=f'{self.module_name}, Process Input Tensors Before Offload ({state})')
         if smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode3:
-            y = y.transpose(-1, -2).contiguous()
+            pass
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode4:
             x = self.lsc.Get_Tensor_Int32(x)
             y = self.lsc.Get_Tensor_Int32(y)
-            y = y.transpose(-1, -2).contiguous()
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode5:
             x_copy = x # copy it so that can be passed to Decryption Key generation
             y_copy = y # copy it so that can be passed to Decryption Key generation
             x, y, blind_factor_u_id, blind_factor_v_id = self.lsc.Get_Encrypted_Tensor_Opr2_Int32(x, y)
-            y = y.transpose(-1, -2).contiguous()
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode6:
             x_copy = x # copy it so that can be passed to Decryption Key generation
             y_copy = y # copy it so that can be passed to Decryption Key generation
             x, y, blind_factor_u_id, blind_factor_v_id = self.sgx_lsc.Get_Encrypted_Tensor_Opr2_Int32(x, y)
-            y = y.transpose(-1, -2).contiguous() 
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode7:
             x_copy = x
             y_copy = y
-            x, y, blind_factor_u_id, blind_factor_v_id = self.sgx_lsc.Get_Encrypted_Tensor_Opr2_Int32(x, y)
-            y = y.transpose(-1, -2).contiguous()
+
+            if self.is_pv_bmm:
+                x, y = self.lsc.Get_Encrypted_Tensor_PV_Int32_KV_Cache_Opt(x, y)
+            else:
+                x, y = self.lsc.Get_Encrypted_Tensor_QK_Int32_KV_Cache_Opt(x, y)
         else:
             assert False
         timer.end(t)
@@ -111,17 +116,37 @@ class BMM_S8X_S8Y_FP32Z_Mixed:
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode6:
             decryption_key_id = self.sgx_lsc.Generate_Decryption_Key_Opr2_Int32(x_copy, y_copy, blind_factor_u_id, blind_factor_v_id)
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode7:
-            decryption_key_id = self.sgx_lsc.Generate_Decryption_Key_Opr2_Int32(x_copy, y_copy, blind_factor_u_id, blind_factor_v_id)
+            if self.is_pv_bmm:
+                decryption_key_id = self.lsc.Generate_Decryption_Key_PV_Int32_KV_Cache_Opt(x_copy, y_copy)
+            else:
+                decryption_key_id = self.lsc.Generate_Decryption_Key_QK_Int32_KV_Cache_Opt(x_copy, y_copy)
         else:
             assert False
+
         timer.end(t)
-
         # Main computation
-
         torch.cuda.synchronize()
         t = timer.start(tag=f'{self.module_name}, Host to Device ({state})', category=f'{self.module_name}, Host to Device ({state})')
         x = x.to(torch.device('cuda:0'))
-        y = y.to(torch.device('cuda:0'))
+        y = y.to(torch.device('cuda:0')) # already transposed
+        torch.cuda.synchronize()
+        timer.end(t)
+
+        torch.cuda.synchronize()
+        t = timer.start(tag=f'{self.module_name}, Manage Y ({state})', category=f'{self.module_name}, Manage Y ({state})')
+
+        y = y.transpose(1, 2).contiguous()
+        if smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode7:
+            if self.cache is None:
+                self.cache = y
+            else:
+                # print("cache shape vs y shape", self.cache.shape, y.shape)
+                if self.is_pv_bmm:
+                    self.cache = torch.cat([self.cache, y], dim=1)
+                else:
+                    self.cache = torch.cat([self.cache, y], dim=2)
+            y = self.cache
+        
         torch.cuda.synchronize()
         timer.end(t)
 
@@ -141,7 +166,6 @@ class BMM_S8X_S8Y_FP32Z_Mixed:
         z = z.to(torch.device('cpu'))
         torch.cuda.synchronize()
         timer.end(t)
-
         t = timer.start(tag=f'{self.module_name}, Process Output Tensors After Offload ({state})', category=f'{self.module_name}, Process Output Tensors After Offload ({state})')
         if smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode4:
             z = self.lsc.Set_Tensor_Int32(z)
@@ -150,11 +174,14 @@ class BMM_S8X_S8Y_FP32Z_Mixed:
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode6:
             z = self.sgx_lsc.Set_Decrypted_Tensor_Opr2_Int32(z, decryption_key_id)
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode7:
-            z = self.sgx_lsc.Set_Decrypted_Tensor_Opr2_Int32(z, decryption_key_id)
+            if self.is_pv_bmm:
+                z = self.lsc.Set_Decrypted_Tensor_PV_Int32_KV_Cache_Opt(z, decryption_key_id)
+            else:
+                z = self.lsc.Set_Decrypted_Tensor_QK_Int32_KV_Cache_Opt(z, decryption_key_id)
         else:
             assert False
         timer.end(t)
-
+        
         t = timer.start(tag=f'{self.module_name}, Compute Epilogue ({state})', category=f'{self.module_name}, Compute Epilogue ({state})')
         if smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode3:
             z = z.to(torch.float32)
@@ -166,11 +193,10 @@ class BMM_S8X_S8Y_FP32Z_Mixed:
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode6:
             z = self.sgx_lsc.Compute_Epilogue_BMM(z, self.bmm_id)
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode7:
-            z = self.sgx_lsc.Compute_Epilogue_BMM(z, self.bmm_id)
+            z = self.lsc.Compute_Epilogue_BMM(z, self.bmm_id)
         else:
             assert False
         timer.end(t)
-
         return z
 
     def __call__(self, x, y):
@@ -179,6 +205,9 @@ class BMM_S8X_S8Y_FP32Z_Mixed:
         end_time = time.perf_counter_ns()
         dt = (end_time - start_time) / 1e9
         return y, dt
+    
+    def cache_len(self):
+        return self.cache.shape[2] if self.cache is not None else 0
 
 
 class BMM_S8X_S8Y_S8Z_Mixed(BMM_S8X_S8Y_FP32Z_Mixed):
@@ -197,7 +226,8 @@ class BMM_S8X_S8Y_S8Z_Mixed(BMM_S8X_S8Y_FP32Z_Mixed):
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode6:
             return self.sgx_lsc.Cast_From_Float_To_Int8(super()._BMM_S8X_S8Y_FP32Z_Mixed__run(x, y))
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode7:
-            return self.sgx_lsc.Cast_From_Float_To_Int8(super()._BMM_S8X_S8Y_FP32Z_Mixed__run(x, y))
+            return self.lsc.Cast_From_Float_To_Int8(super()._BMM_S8X_S8Y_FP32Z_Mixed__run(x, y))
+            
 
     def __call__(self, x, y):
         start_time = time.perf_counter_ns()

smoothquant/my_linear.py
@@ -27,7 +27,7 @@ class Linear_S8W_S8A_S8B_FP32O_Mixed:
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode6:
             self.sgx_lsc = sgx_lsc.SgxLayerStructC()
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode7:
-            self.sgx_lsc = sgx_lsc.SgxLayerStructC()
+            self.lsc = lsc.LayerStructC()
         else:
             assert False
 
@@ -74,7 +74,7 @@ class Linear_S8W_S8A_S8B_FP32O_Mixed:
                 torch.int32).transpose(-2, -1).contiguous().to(torch.device('cuda:0')))  # Send to GPU
             # weight 2D
             # bias 1D
-            self.linear_layer_id = self.sgx_lsc.Set_Linear_Param_WS8BS8(
+            self.linear_layer_id = self.lsc.Set_Linear_Param_WS8BS8(
                 torch_int_nn_linear)
         else:
             assert False
@@ -95,7 +95,7 @@ class Linear_S8W_S8A_S8B_FP32O_Mixed:
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode6:
             x = self.sgx_lsc.Cast_From_Int8_To_Int32(x)
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode7:
-            x = self.sgx_lsc.Cast_From_Int8_To_Int32(x)
+            x = self.lsc.Cast_From_Int8_To_Int32(x)
         else:
             assert False
         timer.end(t)
@@ -112,7 +112,7 @@ class Linear_S8W_S8A_S8B_FP32O_Mixed:
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode6:
             x = self.sgx_lsc.Get_Encrypted_Tensor_Opr1_Int32(x, self.linear_layer_id)
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode7:
-            x = self.sgx_lsc.Get_Encrypted_Tensor_Opr1_Int32(x, self.linear_layer_id)
+            x = self.lsc.Get_Encrypted_Tensor_Opr1_Int32(x, self.linear_layer_id)
         else:
             assert False
         timer.end(t)
@@ -172,7 +172,7 @@ class Linear_S8W_S8A_S8B_FP32O_Mixed:
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode6:
             y = self.sgx_lsc.Set_Decrypted_Tensor_Opr1_Int32(y, self.linear_layer_id)
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode7:
-            y = self.sgx_lsc.Set_Decrypted_Tensor_Opr1_Int32(y, self.linear_layer_id)
+            y = self.lsc.Set_Decrypted_Tensor_Opr1_Int32(y, self.linear_layer_id)
         else:
             assert False
         timer.end(t)
@@ -191,7 +191,7 @@ class Linear_S8W_S8A_S8B_FP32O_Mixed:
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode6:
             y = self.sgx_lsc.Compute_Epilogue_WS8BS8(y, self.linear_layer_id)
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode7:
-            y = self.sgx_lsc.Compute_Epilogue_WS8BS8(y, self.linear_layer_id)
+            y = self.lsc.Compute_Epilogue_WS8BS8(y, self.linear_layer_id)
         else:
             assert False
         timer.end(t)
@@ -222,7 +222,7 @@ class Linear_S8W_S8A_S8B_S8O_Mixed(Linear_S8W_S8A_S8B_FP32O_Mixed):
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode6:
             return self.sgx_lsc.Cast_From_Float_To_Int8(super()._Linear_S8W_S8A_S8B_FP32O_Mixed__run(x))
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode7:
-            return self.sgx_lsc.Cast_From_Float_To_Int8(super()._Linear_S8W_S8A_S8B_FP32O_Mixed__run(x))
+            return self.lsc.Cast_From_Float_To_Int8(super()._Linear_S8W_S8A_S8B_FP32O_Mixed__run(x))
         else:
             assert False
 
@@ -247,7 +247,7 @@ class Linear_S8W_S8A_FP32B_FP32O_Mixed:
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode6:
             self.sgx_lsc = sgx_lsc.SgxLayerStructC()
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode7:
-            self.sgx_lsc = sgx_lsc.SgxLayerStructC()
+            self.lsc = lsc.LayerStructC()
         else:
             assert False
 
@@ -282,7 +282,7 @@ class Linear_S8W_S8A_FP32B_FP32O_Mixed:
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode7:
             self.weight = cupy.from_dlpack(torch_int_nn_linear.weight.to(
                 torch.int32).transpose(-2, -1).contiguous().to(torch.device('cuda:0')))
-            self.linear_layer_id = self.sgx_lsc.Set_Linear_Param_WS8BFP32(
+            self.linear_layer_id = self.lsc.Set_Linear_Param_WS8BFP32(
                 torch_int_nn_linear)
 
     def __run(self, x):
@@ -301,7 +301,7 @@ class Linear_S8W_S8A_FP32B_FP32O_Mixed:
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode6:
             x = self.sgx_lsc.Cast_From_Int8_To_Int32(x)
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode7:
-            x = self.sgx_lsc.Cast_From_Int8_To_Int32(x)
+            x = self.lsc.Cast_From_Int8_To_Int32(x)
         else:
             assert False
         timer.end(t)
@@ -318,7 +318,7 @@ class Linear_S8W_S8A_FP32B_FP32O_Mixed:
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode6:
             x = self.sgx_lsc.Get_Encrypted_Tensor_Opr1_Int32(x, self.linear_layer_id)
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode7:
-            x = self.sgx_lsc.Get_Encrypted_Tensor_Opr1_Int32(x, self.linear_layer_id)
+            x = self.lsc.Get_Encrypted_Tensor_Opr1_Int32(x, self.linear_layer_id)
         else:
             assert False
         timer.end(t)
@@ -379,7 +379,7 @@ class Linear_S8W_S8A_FP32B_FP32O_Mixed:
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode6:
             y = self.sgx_lsc.Set_Decrypted_Tensor_Opr1_Int32(y, self.linear_layer_id)
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode7:
-            y = self.sgx_lsc.Set_Decrypted_Tensor_Opr1_Int32(y, self.linear_layer_id)
+            y = self.lsc.Set_Decrypted_Tensor_Opr1_Int32(y, self.linear_layer_id)
         else:
             assert False
         timer.end(t)
@@ -398,7 +398,7 @@ class Linear_S8W_S8A_FP32B_FP32O_Mixed:
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode6:
             y = self.sgx_lsc.Compute_Epilogue_WS8BFP32(y, self.linear_layer_id)
         elif smoothquant.opt.my_exec_mode == smoothquant.opt.ExecMode.Mode7:
-            y = self.sgx_lsc.Compute_Epilogue_WS8BFP32(y, self.linear_layer_id)
+            y = self.lsc.Compute_Epilogue_WS8BFP32(y, self.linear_layer_id)
         else:
             assert False
         timer.end(t)

smoothquant/opt.py
@@ -217,7 +217,7 @@ class Int8OPTAttention(nn.Module):
         elif my_exec_mode == ExecMode.Mode6:
             self.sgx_lsc = sgx_lsc.SgxLayerStructC()
         elif my_exec_mode == ExecMode.Mode7:
-            self.sgx_lsc = sgx_lsc.SgxLayerStructC()
+            self.lsc = lsc.LayerStructC()
 
     def pre_init(self):
         privacy_on = True if my_exec_mode.value >= ExecMode.Mode5.value else False
@@ -234,6 +234,9 @@ class Int8OPTAttention(nn.Module):
         self.my_qk_bmm = my_bmm.BMM_S8X_S8Y_FP32Z_Mixed(
             self.qk_bmm, privacy_on, module_name="QK BMM")
         self.my_pv_bmm = my_bmm.BMM_S8X_S8Y_S8Z_Mixed(self.pv_bmm, privacy_on, module_name="PV BMM")
+
+        if my_exec_mode == ExecMode.Mode7:
+            self.lsc.Pre_Init() # Reset internal KV cache metadata
 
     def _shape(self, tensor: torch.Tensor, seq_len: int, bsz: int):
         return (
@@ -271,7 +274,7 @@ class Int8OPTAttention(nn.Module):
         elif my_exec_mode == ExecMode.Mode6:
             bsz, tgt_len, _ = self.sgx_lsc.Get_Tensor_Dim_Int8(hidden_states)
         elif my_exec_mode == ExecMode.Mode7:
-            bsz, tgt_len, _ = self.sgx_lsc.Get_Tensor_Dim_Int8(hidden_states)
+            bsz, tgt_len, _ = self.lsc.Get_Tensor_Dim_Int8(hidden_states)
         else:
             assert False
         timer.end(t)
@@ -398,14 +401,21 @@ class Int8OPTAttention(nn.Module):
             elif my_exec_mode == ExecMode.Mode7:
                 key_states, my_k_proj_dt = self.my_k_proj(hidden_states)
                 value_states, my_v_proj_dt = self.my_v_proj(hidden_states)
+                # output size is [1, 1, 768]
 
                 t = timer.start(
                     tag=f'Construct KV Cache ({state})', category=f'Construct KV Cache ({state})')
-                key_states = self.sgx_lsc.Get_Tensor_Int8(key_states)
-                value_states = self.sgx_lsc.Get_Tensor_Int8(value_states)
+                key_states = self.lsc.Get_Tensor_Int8(key_states)
+                value_states = self.lsc.Get_Tensor_Int8(value_states)
 
                 key_states = self._shape(key_states, -1, bsz)
                 value_states = self._shape(value_states, -1, bsz)
+
+                actual_key_states = key_states
+                actual_value_states = value_states
+
+                # Actual Concatenation is managed on GPU side after encryption
+                # This is concatenated to follow naive OPT
                 key_states = torch.cat([past_key_value[0], key_states], dim=2)
                 value_states = torch.cat(
                     [past_key_value[1], value_states], dim=2)
@@ -485,11 +495,14 @@ class Int8OPTAttention(nn.Module):
 
                 t = timer.start(
                     tag=f'Construct KV Cache ({state})', category=f'Construct KV Cache ({state})')
-                key_states = self.sgx_lsc.Get_Tensor_Int8(key_states)
-                value_states = self.sgx_lsc.Get_Tensor_Int8(value_states)
+                key_states = self.lsc.Get_Tensor_Int8(key_states)
+                value_states = self.lsc.Get_Tensor_Int8(value_states)
 
                 key_states = self._shape(key_states, -1, bsz)
                 value_states = self._shape(value_states, -1, bsz)
+
+                actual_key_states = key_states
+                actual_value_states = value_states
                 timer.end(t)
             else:
                 assert False
@@ -497,6 +510,7 @@ class Int8OPTAttention(nn.Module):
         t = timer.start(tag=f'Get Past KV ({state})',
                         category=f'Get Past KV ({state})')
         past_key_value = (key_states, value_states)
+        # seems like past_key_value exists for each layer
         timer.end(t)
 
         t = timer.start(tag=f'Reshape Q, K, V ({state})',
@@ -514,12 +528,16 @@ class Int8OPTAttention(nn.Module):
         elif my_exec_mode == ExecMode.Mode6:
             query_states = self.sgx_lsc.Get_Tensor_Int8(query_states)
         elif my_exec_mode == ExecMode.Mode7:
-            query_states = self.sgx_lsc.Get_Tensor_Int8(query_states)
+            query_states = self.lsc.Get_Tensor_Int8(query_states)
         
         query_states = self._shape(
             query_states, tgt_len, bsz).view(*proj_shape)
         key_states = key_states.view(*proj_shape)
         value_states = value_states.view(*proj_shape)
+
+        if my_exec_mode == ExecMode.Mode7:
+            actual_key_states = actual_key_states.view(*proj_shape)
+            actual_value_states = actual_value_states.view(*proj_shape)
 
         src_len = key_states.size(1)
 
@@ -537,8 +555,10 @@ class Int8OPTAttention(nn.Module):
             query_states = self.sgx_lsc.Set_Tensor_Int8(query_states)
             key_states = self.sgx_lsc.Set_Tensor_Int8(key_states)
         elif my_exec_mode == ExecMode.Mode7:
-            query_states = self.sgx_lsc.Set_Tensor_Int8(query_states)
-            key_states = self.sgx_lsc.Set_Tensor_Int8(key_states)
+            query_states = self.lsc.Set_Tensor_Int8(query_states)
+            # During generation, set only the recent one
+            # That means encrypted past key states must be kept inside QK_BMM
+            actual_key_states = self.lsc.Set_Tensor_Int8(actual_key_states)
         else:
             assert False
         timer.end(t)
@@ -570,8 +590,8 @@ class Int8OPTAttention(nn.Module):
             attn_weights = self.sgx_lsc.Get_Tensor_Float(attn_weights)
         elif my_exec_mode == ExecMode.Mode7:
             attn_weights, my_qk_bmm_dt = self.my_qk_bmm(
-                query_states, key_states)
-            attn_weights = self.sgx_lsc.Get_Tensor_Float(attn_weights)
+                query_states, actual_key_states)
+            attn_weights = self.lsc.Get_Tensor_Float(attn_weights)
         else:
             assert False
 
@@ -587,6 +607,9 @@ class Int8OPTAttention(nn.Module):
             NOTE(jpyo0803): Add attention_mask to attn_weights.
             Should be done in CPU side SGX, O(N^2)
         '''
+        if my_exec_mode == ExecMode.Mode7:
+            src_len = self.my_qk_bmm.cache_len() # size of last dim of key_states
+
         if attention_mask is not None:
             if attention_mask.size() != (bsz, 1, tgt_len, src_len):
                 raise ValueError(
@@ -627,9 +650,9 @@ class Int8OPTAttention(nn.Module):
             attn_probs = self.sgx_lsc.Softmax(attn_weights)
             attn_probs = self.sgx_lsc.Get_Tensor_Float(attn_probs)
         elif my_exec_mode == ExecMode.Mode7:
-            attn_weights = self.sgx_lsc.Set_Tensor_Float(attn_weights)
-            attn_probs = self.sgx_lsc.Softmax(attn_weights)
-            attn_probs = self.sgx_lsc.Get_Tensor_Float(attn_probs)
+            attn_weights = self.lsc.Set_Tensor_Float(attn_weights)
+            attn_probs = self.lsc.Softmax(attn_weights)
+            attn_probs = self.lsc.Get_Tensor_Float(attn_probs)
         else:
             assert False
         timer.end(t)
@@ -688,21 +711,25 @@ class Int8OPTAttention(nn.Module):
             attn_probs = self.sgx_lsc.Set_Tensor_Float(attn_probs)
             attn_probs = self.sgx_lsc.Quantize_Post_Softmax(attn_probs)
         elif my_exec_mode == ExecMode.Mode7:
-            attn_probs = self.sgx_lsc.Set_Tensor_Float(attn_probs)
-            attn_probs = self.sgx_lsc.Quantize_Post_Softmax(attn_probs)
+            attn_probs = self.lsc.Set_Tensor_Float(attn_probs)
+            attn_probs = self.lsc.Quantize_Post_Softmax(attn_probs)
         else:
             assert False
         timer.end(t)
 
         t = timer.start(tag=f'Transpose V ({state})',
                         category=f'Transpose V ({state})')
-        value_states = value_states.transpose(1, 2).contiguous()
+        if my_exec_mode == ExecMode.Mode7:
+            actual_value_states = actual_value_states.transpose(1, 2).contiguous()
+        else:
+            value_states = value_states.transpose(1, 2).contiguous()
         timer.end(t)
 
         '''
             NOTE(jpyo0803): Pass attn_probs and value_states to pv_bmm.
             Matmul should be done on Untrusted GPU side
         '''
+        
         if my_exec_mode == ExecMode.Mode1:
             t = timer.start(tag=f'PV BMM ({state})',
                             category=f'PV BMM ({state})')
@@ -727,14 +754,15 @@ class Int8OPTAttention(nn.Module):
                 attn_probs, value_states)
             attn_output = self.sgx_lsc.Get_Tensor_Int8(attn_output)
         elif my_exec_mode == ExecMode.Mode7:
-            value_states = self.sgx_lsc.Set_Tensor_Int8(value_states)
+            actual_value_states = self.lsc.Set_Tensor_Int8(actual_value_states)
             attn_output, my_pv_bmm_dt = self.my_pv_bmm(
-                attn_probs, value_states)
-            attn_output = self.sgx_lsc.Get_Tensor_Int8(attn_output)
+                attn_probs, actual_value_states)
+            attn_output = self.lsc.Get_Tensor_Int8(attn_output)
         else:
             assert False
 
-
+        # output size should be [12, 1024, 64]
+        
         t = timer.start(tag=f'Reshape Attention Output ({state})',
                         category=f'Reshape Attention Output ({state})')
         if attn_output.size() != (bsz * self.num_heads, tgt_len, self.head_dim):
@@ -742,7 +770,6 @@ class Int8OPTAttention(nn.Module):
                 f"`attn_output` should be of size {(bsz, self.num_heads, tgt_len, self.head_dim)}, but is"
                 f" {attn_output.size()}"
             )
-
         attn_output = attn_output.view(
             bsz, self.num_heads, tgt_len, self.head_dim)
         attn_output = attn_output.transpose(1, 2)
@@ -776,7 +803,7 @@ class Int8OPTAttention(nn.Module):
             attn_output = self.sgx_lsc.Set_Tensor_Int8(attn_output)
             attn_output, my_out_proj_dt = self.my_out_proj(attn_output)
         elif my_exec_mode == ExecMode.Mode7:
-            attn_output = self.sgx_lsc.Set_Tensor_Int8(attn_output)
+            attn_output = self.lsc.Set_Tensor_Int8(attn_output)
             attn_output, my_out_proj_dt = self.my_out_proj(attn_output)
         else:
             assert False
@@ -810,7 +837,7 @@ class Int8OPTDecoderLayer(nn.Module):
         elif my_exec_mode == ExecMode.Mode6:
             self.sgx_lsc = sgx_lsc.SgxLayerStructC()
         elif my_exec_mode == ExecMode.Mode7:
-            self.sgx_lsc = sgx_lsc.SgxLayerStructC()
+            self.lsc = lsc.LayerStructC()
 
     def pre_init(self):
         # how to compare enum by value?
@@ -838,9 +865,9 @@ class Int8OPTDecoderLayer(nn.Module):
             self.final_layer_norm_id = self.sgx_lsc.Set_Layer_Norm_Param(
                 self.final_layer_norm)
         elif my_exec_mode == ExecMode.Mode7:
-            self.self_attn_layer_norm_id = self.sgx_lsc.Set_Layer_Norm_Param(
+            self.self_attn_layer_norm_id = self.lsc.Set_Layer_Norm_Param(
                 self.self_attn_layer_norm)
-            self.final_layer_norm_id = self.sgx_lsc.Set_Layer_Norm_Param(
+            self.final_layer_norm_id = self.lsc.Set_Layer_Norm_Param(
                 self.final_layer_norm)
 
         self.self_attn.pre_init()
@@ -902,7 +929,7 @@ class Int8OPTDecoderLayer(nn.Module):
             hidden_states = self.sgx_lsc.Set_Hidden_States(
                 hidden_states)  # pass
         elif my_exec_mode == ExecMode.Mode7:
-            hidden_states = self.sgx_lsc.Set_Hidden_States(
+            hidden_states = self.lsc.Set_Hidden_States(
                 hidden_states)  # pass
         else:
             assert False
@@ -922,7 +949,7 @@ class Int8OPTDecoderLayer(nn.Module):
         elif my_exec_mode == ExecMode.Mode6:
             residual = self.sgx_lsc.Copy_Hidden_States(hidden_states)  # pass
         elif my_exec_mode == ExecMode.Mode7:
-            residual = self.sgx_lsc.Copy_Hidden_States(hidden_states)
+            residual = self.lsc.Copy_Hidden_States(hidden_states)
         else:
             assert False
         timer.end(t)
@@ -951,7 +978,7 @@ class Int8OPTDecoderLayer(nn.Module):
             hidden_states = self.sgx_lsc.Layer_Norm_Q(
                 hidden_states, self.self_attn_layer_norm_id)  # id (float) -> id (int8)
         elif my_exec_mode == ExecMode.Mode7:
-            hidden_states = self.sgx_lsc.Layer_Norm_Q(
+            hidden_states = self.lsc.Layer_Norm_Q(
                 hidden_states, self.self_attn_layer_norm_id)
         else:
             assert False
@@ -996,7 +1023,7 @@ class Int8OPTDecoderLayer(nn.Module):
             # hidden_states = self.lsc.Set_Tensor_Float(hidden_states)
             hidden_states = self.sgx_lsc.Residual_Add(residual, hidden_states)
         elif my_exec_mode == ExecMode.Mode7:
-            hidden_states = self.sgx_lsc.Residual_Add(residual, hidden_states)  
+            hidden_states = self.lsc.Residual_Add(residual, hidden_states)  
         else:
             assert False
         timer.end(t)
@@ -1019,7 +1046,7 @@ class Int8OPTDecoderLayer(nn.Module):
         elif my_exec_mode == ExecMode.Mode6:
             residual = self.sgx_lsc.Copy_Hidden_States(hidden_states)
         elif my_exec_mode == ExecMode.Mode7:
-            residual = self.sgx_lsc.Copy_Hidden_States(hidden_states)
+            residual = self.lsc.Copy_Hidden_States(hidden_states)
         else:
             assert False
         timer.end(t)
@@ -1048,7 +1075,7 @@ class Int8OPTDecoderLayer(nn.Module):
             hidden_states = self.sgx_lsc.Layer_Norm_Q(
                 hidden_states, self.final_layer_norm_id)
         elif my_exec_mode == ExecMode.Mode7:
-            hidden_states = self.sgx_lsc.Layer_Norm_Q(
+            hidden_states = self.lsc.Layer_Norm_Q(
                 hidden_states, self.final_layer_norm_id)
         else:
             assert False
@@ -1124,8 +1151,8 @@ class Int8OPTDecoderLayer(nn.Module):
             hidden_states, my_fc1_dt = self.my_fc1(hidden_states)
 
             t = timer.start(tag=f'ReLU ({state})', category=f'ReLU ({state})')
-            hidden_states = self.sgx_lsc.ReLU(hidden_states)
-            hidden_states = self.sgx_lsc.Cast_From_Float_To_Int8(hidden_states)
+            hidden_states = self.lsc.ReLU(hidden_states)
+            hidden_states = self.lsc.Cast_From_Float_To_Int8(hidden_states)
             timer.end(t)
         else:
             assert False
@@ -1183,8 +1210,8 @@ class Int8OPTDecoderLayer(nn.Module):
             residual = self.sgx_lsc.Residual_Add(residual, hidden_states)
             residual = self.sgx_lsc.Get_Tensor_Float(residual)
         elif my_exec_mode == ExecMode.Mode7:
-            residual = self.sgx_lsc.Residual_Add(residual, hidden_states)
-            residual = self.sgx_lsc.Get_Tensor_Float(residual)
+            residual = self.lsc.Residual_Add(residual, hidden_states)
+            residual = self.lsc.Get_Tensor_Float(residual)
         else:
             assert False
         timer.end(t)

smoothquant_eval.py
@@ -66,6 +66,8 @@ class Evaluator:
         end = torch.cuda.Event(enable_timing=True)
         latency = 0
         for i, batch in enumerate(self.dataset):
+            model.pre_init()
+
             print(f'Processing {i+1}-th Batch')
             if start_gpu:
                 input_ids = batch['input_ids'].cuda().unsqueeze(0)
@@ -119,7 +121,6 @@ print("Start Device: ", "CUDA" if start_gpu else "CPU")
 model_smoothquant = Int8OPTForCausalLM.from_pretrained(
     'mit-han-lab/opt-125m-smoothquant', torch_dtype=torch.float32, device_map='cuda:0' if start_gpu else 'cpu')
 
-model_smoothquant.pre_init()  # Need this to initialize the model weights in CUDA
 
 
 print_model_size(model_smoothquant)